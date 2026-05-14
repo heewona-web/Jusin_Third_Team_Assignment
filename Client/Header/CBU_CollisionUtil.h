7bfda60985355c860c27620900a2026631832232
임병언 <onion@onyoutour.com>
2026-05-14 18:03:18 +0900

    feat(BU): 프로젝트 구조 잡기 2

--- Client/Header/CBU_CollisionUtil.h
+++ Client/Header/CBU_CollisionUtil.h
@@ -1,5 +1,7 @@
 #pragma once
+
 class CBU_CollisionUtil
 {
+
 };
 
--- Client/Header/CBU_Object.h
+++ Client/Header/CBU_Object.h
@@ -1,9 +1,20 @@
 #pragma once
 #include "CBase.h"
+#include "Define.h"
 
 class CBU_Object : public CBase
 {
-	explicit CBU_Object();
-	virtual ~CBU_Object();
+public:
+	explicit CBU_Object() {}
+	virtual ~CBU_Object() {}
+
+	virtual void Initialize(void) PURE;
+	virtual void Update(void) PURE;
+	virtual void LateUpdate(void) PURE;
+	virtual void Render(HDC hDC) const PURE;
+	virtual void Release(void) PURE;
+
+protected:
+	INFO m_tInfo;
 };
 
