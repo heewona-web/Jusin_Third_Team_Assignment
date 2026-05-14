10241dd66e8d0256fb18fc823b8e99283fd9a9b9
임병언 <onion@onyoutour.com>
2026-05-14 17:38:45 +0900

    feat: 규약위반 CTest 제거 및 초기 프로젝트 구조 잡기

---
+++ Client/Header/CBU_MainGame.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "CBase.h"
+
+class CBU_MainGame : public CBase
+{
+public:
+	explicit CBU_MainGame();
+	virtual ~CBU_MainGame();
+
+
+};
+
---
+++ Client/Header/CBU_Object.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "CBase.h"
+
+class CBU_Object : public CBase
+{
+	explicit CBU_Object();
+	virtual ~CBU_Object();
+};
+
